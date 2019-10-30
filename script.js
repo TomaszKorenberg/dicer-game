8be7055a4fb2245249ebe7df0820107dc849e6c3
Tomasz Korenberg <info@casestudio.pl>
2019-10-30 21:30:29 +0100

    dodane setTimeout celem przypisania wyniku z serwera do zmiennej diceNumber

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+let diceNumber = 0;
+
 DicerApi = function () {
     this.url = "https://api.random.org/json-rpc/1/invoke";
+    this.jsonrpc = "2.0";
+    this.method = "generateIntegers";
+    this.id = "tomaszKorenberg";
 };
 
 
 DicerApi.prototype.post = function (){
     const url = this.url;
-    const jsonrpc = "2.0";
-    const method = "generateIntegers";
-    const id = "tomaszKorenberg";
+    const jsonrpc = this.jsonrpc;
+    const method = this.method;
+    const id = this.id;
     const params = {
-        "apiKey" : "96b6bb22-88d6-485b-9491-3299309f9f80",
-        "n" : 1,
-        "min" : 1,
-        "max" : 6
+        "apiKey" : "96b6bb22-88d6-485b-9491-3299309f9f80",  //api key to random.org
+        "n" : 1,    //number of numbers to draw
+        "min" : 1,  //from this number
+        "max" : 6   //to this number
     }
 
     return fetch(url, {
@@ -25,12 +30,16 @@ DicerApi.prototype.post = function (){
         headers: {
             "Accept" : "application/json",
             "Content-Type" : "application/json"
-        }}).then(response => response.json()).then( response => console.log(response.result.random.data[0]));
+        }}).then(response => response.json()).then(response => diceNumber = response.result.random.data[0]);
 };
 
 
 const dicerApi = new DicerApi();
 
-;
 
-dicerApi.post();
+//----------czekamy aż serwer zwróci odpowiedź i dopiero ją logujemy do konsoli
+diceNumber = dicerApi.post();
+setTimeout(function(){
+    console.log(diceNumber);
+}, 600);
+//----------------------------------------
