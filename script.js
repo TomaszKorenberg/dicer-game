a903bfcba27cf4f773abc28966b10f901e8a76a0
Tomasz Korenberg <info@casestudio.pl>
2019-11-03 16:37:57 +0100

    dodane setTimeout celem przypisania wyniku z serwera do zmiennej diceNumber

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ DicerApi = function () {
     this.jsonrpc = "2.0";
     this.method = "generateIntegers";
     this.id = "tomaszKorenberg";
+    this.imageContainer = document.querySelector("#image-container");
+
 };
 
 
@@ -18,7 +20,7 @@ DicerApi.prototype.post = function (){
         "n" : 1,    //number of numbers to draw
         "min" : 1,  //from this number
         "max" : 6   //to this number
-    }
+    };
 
     return fetch(url, {
         method: "POST",
@@ -33,13 +35,28 @@ DicerApi.prototype.post = function (){
         }}).then(response => response.json()).then(response => diceNumber = response.result.random.data[0]);
 };
 
+DicerApi.prototype.diceImage = function(number){
+    return "img/dice" + number + ".png"
+};
+
 
 const dicerApi = new DicerApi();
 
 
-//----------czekamy aż serwer zwróci odpowiedź i dopiero ją logujemy do konsoli
-diceNumber = dicerApi.post();
-setTimeout(function(){
-    console.log(diceNumber);
-}, 600);
-//----------------------------------------
+
+DicerApi.prototype.runDraw = function(){
+
+    diceNumber = dicerApi.post();
+    this.imageContainer.innerHTML = "Losowanie..." ;
+
+    setTimeout(function(){
+        this.imageContainer.innerHTML = "<img src=\"" + dicerApi.diceImage(diceNumber) + "\">";
+    }.bind(this), 1700);
+
+};
+
+const button = document.querySelector("#draw-button");
+
+button.addEventListener("click", function(){
+    dicerApi.runDraw();
+});
